48b47a1c2568ea11982fabfa15528df4c55724a4
VOLODYMYR KACHUR <kachurslavik123@gmail.com>
2026-04-26 13:22:49 +0300

    delete

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import { getIce } from "./api/get-ice";
 import { postIce } from "./api/post-ice";
+import { delIce } from "./api/del-ice";
 
 const listRef = document.querySelector(".list");
 const modalBtn = document.querySelector(".modal-btn");
@@ -16,8 +17,8 @@ function createItemsMarcups(array) {
         <p>${price}</p>
         <p>${calories}</p>
         <p>${type}</p>
-        <button type="button" class="edit">Edit</button>
-        <button type="button" class="delete">Delete</button>
+        <button type="button" class="edit" data-action="edit">Edit</button>
+        <button type="button" class="delete" data-action="delete">Delete</button>
     </li>`;
     })
     .join("");
@@ -50,10 +51,26 @@ form.addEventListener("submit", (event) => {
     type: elements.type.value,
   };
   postIce(iceData).then(() => {
-    getIce().then(res => {
-        form.reset()
-        closeModal()
-        createItemsMarcups(res)
-    })
-  })
+    getIce().then((res) => {
+      form.reset();
+      closeModal();
+      createItemsMarcups(res);
+    });
+  });
 });
+
+listRef.addEventListener("click", (event) => {
+  const action = event.target.dataset.action;
+  
+  if (!action) {
+    return;
+  }
+  const li = event.target.closest("li");
+
+  const id = li.id;
+  if(action === "delete"){
+    delIce(id).then(()=>getIce()).then((res)=>createItemsMarcups(res))
+    // delIce(id).then(getIce).then(res=>createItemsMarcups(res))
+  }
+});
+
